f901940e9c1e464b66d83096d7411b47311521e8
Moises Rivas <mardemza@hotmail.com>
2015-07-01 11:59:27 -0300

    Agrego Clase Player

---
+++ Demos/Demos.ProyectoConsola/Player.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demos
+{
+    public class Player
+    {
+        // Propiedades 
+        public string Name { get; set; }
+        public string Surname { get; set; }
+
+        /// <summary>
+        /// Contructor de la Clase Player
+        /// </summary>
+        /// <param name="name">Variable tipo string de entrada del nombre</param>
+        /// <param name="surname">Variable tipo string de entrada del apellido</param>
+        public Player(string name, string surname)
+        {
+            Name = name;
+            Surname = surname;
+        }
+
+        /// <summary>
+        /// Metodo que retorna el nombre completo
+        /// </summary>
+        /// <returns>Retorna un string con el nombre y el apellido separados por un espacio</returns>
+        public string FullName()
+        {
+            // Contatenar
+            return string.Format("{0} {1}", Name, Surname);
+        }
+    }
+}
--- Demos/Demos.ProyectoConsola/Program.cs
+++ Demos/Demos.ProyectoConsola/Program.cs
@@ -10,6 +10,26 @@ namespace Demos
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("====================================");
+            Console.WriteLine("==== Instanciar Clase Player =======");
+            Console.WriteLine("====================================");
+            Console.WriteLine();
+
+            // Instanciar la clase Player
+            var player = new Player("Juan","Suarez");
+
+            // Retorno el nombre utilizando el metodo Format del tipo string
+            Console.WriteLine(string.Format("Nombre: {0}",player.Name));
+            // Retorno el apellido concatenando los string con el signo +
+            Console.WriteLine("Apellido: " + player.Surname);
+
+            // Retorno el nombre completo utilizando la interpolacion de cadena 
+            Console.WriteLine($"Nombre Completo: {player.FullName()}");
+            Console.WriteLine();
+            Console.WriteLine("====================================");
+            Console.WriteLine();
+
+            Console.ReadKey();
         }
     }
 }
