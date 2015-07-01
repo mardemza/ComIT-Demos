3086bc5a82ae72f7fff86a5825f6e97bb46cdc68
Moises Rivas <mardemza@hotmail.com>
2015-07-01 13:41:33 -0300

    Agregando la clase Team y su implementación

--- Demos/Demos.ProyectoConsola/Program.cs
+++ Demos/Demos.ProyectoConsola/Program.cs
@@ -10,23 +10,40 @@ namespace Demos
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("====================================");
-            Console.WriteLine("==== Instanciar Clase Player =======");
-            Console.WriteLine("====================================");
+            Console.WriteLine("|==================================|");
+            Console.WriteLine("|     Instanciar Clase Player      |");
+            Console.WriteLine("|==================================|");
             Console.WriteLine();
 
             // Instanciar la clase Player
-            var player = new Player("Juan","Suarez");
+            var player = new Player("Lionel","Messi");
 
             // Retorno el nombre utilizando el metodo Format del tipo string
-            Console.WriteLine(string.Format("Nombre: {0}",player.Name));
+            Console.WriteLine(string.Format("  Nombre: {0}",player.Name));
             // Retorno el apellido concatenando los string con el signo +
-            Console.WriteLine("Apellido: " + player.Surname);
+            Console.WriteLine("  Apellido: " + player.Surname);
 
             // Retorno el nombre completo utilizando la interpolacion de cadena 
-            Console.WriteLine($"Nombre Completo: {player.FullName()}");
+            Console.WriteLine($"  Nombre Completo: {player.FullName()}");
+
             Console.WriteLine();
-            Console.WriteLine("====================================");
+            Console.WriteLine("|==================================|");
+            Console.WriteLine("|     Instanciar Clase Team        |");
+            Console.WriteLine("|==================================|");
+            Console.WriteLine();
+
+            var team = new Team() {Name = "Barcelona"};
+
+            // Agrego a la propiedad tipo lista de Player
+            // de la clase Team un jugador ya creado que en este
+            // caso es un objeto de tipo Player
+            team.Players.Add(player);
+
+            // Retorno el nombre del Equipo
+            Console.WriteLine($"  Nombre del Equipo: {team.Name}");
+            // Llamo al metodo GetCount y retorno la cantidad de jugadores
+            Console.WriteLine($"  Cantidad de Jugadores: {team.GetCount()}");
+            
             Console.WriteLine();
 
             Console.ReadKey();
---
+++ Demos/Demos.ProyectoConsola/Team.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demos
+{
+    public class Team
+    {
+        // Propiedades de la clase Team
+        public string Name { get; set; }
+        public List<Player> Players { get; set; }
+
+        /// <summary>
+        /// Constructor de la clase Team
+        /// </summary>
+        public Team()
+        {
+            Players = new List<Player>();
+        }
+
+        /// <summary>
+        /// Metodo para retornar la cantidad de jugadores que tiene la clase Team
+        /// </summary>
+        /// <returns>Valor tipo entero</returns>
+        public int GetCount()
+        {
+            return Players.Count();
+        }
+    }
+}
